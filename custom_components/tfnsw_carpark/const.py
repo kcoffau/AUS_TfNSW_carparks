10e842007a7332ea3739d269399aca876c8d1d93
Kay Cee <35386534+kcoffau@users.noreply.github.com>
2025-05-27 10:54:04 +1000

    Create const.py

---
+++ custom_components/tfnsw_carpark/const.py
@@ -0,0 +1,7 @@
+"""Constants for the TfNSW Car Park integration."""
+DOMAIN = "tfnsw_carpark"
+
+API_BASE_URL = "https://api.transport.nsw.gov.au/v1/carpark"
+
+CONF_API_KEY = "api_key"
+CONF_SELECTED_CARPARKS = "selected_carparks"
